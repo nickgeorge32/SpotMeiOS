09ddfaa4fc81350a6ffaa95ef2314f6403c25630
Nick George <nickgeorge@simpleseonow.com>
2017-03-20 11:46:04 -0500

    fixed links for UserDetailsContVC added keyboard functionality

--- SpotMe/UserDetailsContViewController.swift
+++ SpotMe/UserDetailsContViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class UserDetailsContViewController: UIViewController {
+class UserDetailsContViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet var userHeight: UITextField!
     @IBOutlet var weightGoalSegment: UISegmentedControl!
+    @IBOutlet var goalWeightField: UITextField!
     @IBOutlet var weeklyGoalSegment: UISegmentedControl!
     @IBOutlet var desiredOutcomeSegment: UISegmentedControl!
     @IBOutlet var emailSwitch: UISwitch!
@@ -19,8 +21,15 @@ class UserDetailsContViewController: UIViewController {
     var dob:String!
     var userWeight:String!
     
+    @IBAction func weightGoal(_ sender: Any) {
+        if weightGoalSegment.selectedSegmentIndex == 0 || weightGoalSegment.selectedSegmentIndex == 2 {
+            goalWeightField.isHidden = false
+        } else {
+            goalWeightField.isHidden = true
+        }
+    }
     @IBAction func weeklyGoal(_ sender: Any) {
-        
+
     }
     @IBAction func desiredOutcome(_ sender: Any) {
         
@@ -32,6 +41,8 @@ class UserDetailsContViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        addDoneButtonOnKeyboard()
 
         // Do any additional setup after loading the view.
         print(userGender)
@@ -44,14 +55,38 @@ class UserDetailsContViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func addDoneButtonOnKeyboard()
+    {
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect(x:0,y: 0,width: 320,height: 50))
+        doneToolbar.barStyle = UIBarStyle.default
+        
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.done, target: self, action: #selector(doneButtonAction))
+        
+        var items = [UIBarButtonItem]()
+        items.append(flexSpace)
+        items.append(done)
+        
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+        
+        self.userHeight.inputAccessoryView = doneToolbar
+        
     }
-    */
+    
+    func doneButtonAction()
+    {
+        self.userHeight.resignFirstResponder()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
+    }
 
 }
