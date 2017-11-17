b6ecb1a82e38d8b39bca4e22d9353f37019a8fb0
Nick George <nickgeorge@nicks-mac-mini.local>
2017-11-16 21:18:41 -0600

    finished pageVC for welcome Screen

--- SpotMe/PageViewController.swift
+++ SpotMe/PageViewController.swift
@@ -8,94 +8,50 @@
 
 import UIKit
 
-class PageViewController: UIPageViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
-    func newVc(viewController: String) -> UIViewController {
-        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: viewController)
-    }
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
-            return nil
-        }
-        
-        let previousIndex = viewControllerIndex - 1
-        
-        // User is on the first view controller and swiped left to loop to
-        // the last view controller.
-        guard previousIndex >= 0 else {
-            return orderedViewControllers.last
-            // Uncommment the line below, remove the line above if you don't want the page control to loop.
-            // return nil
-        }
-        
-        guard orderedViewControllers.count > previousIndex else {
-            return nil
-        }
-        
-        return orderedViewControllers[previousIndex]
-    }
+class PageViewController: UIPageViewController {
+    var pageTitles = ["Lift More", "Go Farther", "Set Goals", "Compete"]
+    var bgImages = ["lift_more", "go_farther", "goals", "compete"]
+    var pageDescriptions = ["Never workout alone again! Discover your new cycling, running, lifting, swimming pal today.", "Never workout alone again! Discover your new cycling, running, lifting, swimming pal today.", "Never workout alone again! Discover your new cycling, running, lifting, swimming pal today.", "Never workout alone again! Discover your new cycling, running, lifting, swimming pal today."]
     
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
-            return nil
-        }
-        
-        let nextIndex = viewControllerIndex + 1
-        let orderedViewControllersCount = orderedViewControllers.count
-        
-        // User is on the last view controller and swiped right to loop to
-        // the first view controller.
-        guard orderedViewControllersCount != nextIndex else {
-            return orderedViewControllers.first
-            // Uncommment the line below, remove the line above if you don't want the page control to loop.
-            // return nil
-        }
-        
-        guard orderedViewControllersCount > nextIndex else {
-            return nil
-        }
-        
-        return orderedViewControllers[nextIndex]
-    }
-    
-
-    lazy var orderedViewControllers: [UIViewController] = {
-        return [self.newVc(viewController: "Welcome1ViewController"),
-                self.newVc(viewController: "Welcome2ViewController"),
-                self.newVc(viewController: "Welcome3ViewController"),
-                self.newVc(viewController: "Welcome4ViewController")]
-    }()
-    
-    var pageControl = UIPageControl()
-    func configurePageControl() {
-        // The total number of pages that are available is based on how many available colors we have.
-        pageControl = UIPageControl(frame: CGRect(x: 0,y: UIScreen.main.bounds.maxY - 200,width: UIScreen.main.bounds.width,height: 50))
-        self.pageControl.numberOfPages = orderedViewControllers.count
-        self.pageControl.currentPage = 0
-        self.pageControl.tintColor = UIColor.gray
-        self.pageControl.pageIndicatorTintColor = UIColor.white
-        self.pageControl.currentPageIndicatorTintColor = UIColor.gray
-        self.view.addSubview(pageControl)
-    }
-    
-    // MARK: Delegate functions
-    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
-        let pageContentViewController = pageViewController.viewControllers![0]
-        self.pageControl.currentPage = orderedViewControllers.index(of: pageContentViewController)!
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.dataSource = self
-        self.delegate = self
-        configurePageControl()
         
-        // This sets up the first view that will show up on our page control
-        if let firstViewController = orderedViewControllers.first {
-            setViewControllers([firstViewController],
-                               direction: .forward,
-                               animated: true,
-                               completion: nil)
+        if let firstVC = self.viewControllerAtIndex(index: 0) {
+            setViewControllers([firstVC], direction: .forward, animated: true, completion: nil)
         }
     }
     
+    //MARK: Navigate    
+    func viewControllerAtIndex(index: Int) -> ViewController? {
+        if (index == NSNotFound || index < 0 || index >= self.pageTitles.count) {
+            return nil
+        }
+        
+        if let viewController = storyboard?.instantiateViewController(withIdentifier: "WelcomeViewController") as? ViewController {
+            viewController.bgImageName = bgImages[index]
+            viewController.titleText = pageTitles[index]
+            viewController.detailsText = pageDescriptions[index]
+            viewController.index = index
+            return viewController
+        }
+        return nil
+    }
 }
+    
+    //MARK: DataSource
+    
+    extension PageViewController: UIPageViewControllerDataSource {
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+            var index = (viewController as! ViewController).index
+            index -= 1
+            return self.viewControllerAtIndex(index: index)
+        }
+        
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+            var index = (viewController as! ViewController).index
+            index += 1
+            return self.viewControllerAtIndex(index: index)
+        }
+    }
